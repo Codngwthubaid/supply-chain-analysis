31a4df8835ad14f6b0375d618d7848ea49af9ec4
codngwthubaid <codngwthubaid@gmail.com>
2026-06-13 09:41:08 +0530

    implement delay and profitability analysis and visualize delay percentage by category

--- main.py
+++ main.py
@@ -129,12 +129,12 @@ df['Profitability Flag'] = np.where(
 
 # create a pie chat for visualization of profitability distribution
 profit_counts = df['Profitability Flag'].value_counts(normalize = True) * 100
-profit_counts.plot(kind = 'pie', 
-    autopct = '%1.1f%%', 
-    startangle = 90, 
-    figsize = (8, 8), 
-    colors = [accent_color, danger_color, secondary_color],
-    wedgeprops = {'edgecolor': 'black', 'linewidth': 1})
+# profit_counts.plot(kind = 'pie', 
+#     autopct = '%1.1f%%', 
+#     startangle = 90, 
+#     figsize = (8, 8), 
+#     colors = [accent_color, danger_color, secondary_color],
+#     wedgeprops = {'edgecolor': 'black', 'linewidth': 1})
 
 # plt.title('Profitability Distribution (%)')
 # plt.savefig('figures/profitability_distribution.png', dpi = 300, bbox_inches = 'tight')
@@ -172,7 +172,7 @@ metrics['Late delivery %'] = (float(metrics['Late Deliveries']) / metrics['Total
 metrics['Total profit'] = format_func(df.loc[df['Order Profit Per Order'] > 0, 'Order Profit Per Order'].sum())
 metrics['Total loss due to delay'] = format_func(df.loc[df['Delay'] > 0, 'Order Profit Per Order'].sum())
 
-print("\n --- Business KPI's --- \n")
+# print("\n --- Business KPI's --- \n")
 # for key, value in metrics.items():
 #     if isinstance(value, float):
 #         print(f'{key} : {value:.2f}')
@@ -181,3 +181,132 @@ print("\n --- Business KPI's --- \n")
 
 
 # now, we find out the profitability VS delivery time analysis
+profit_metrics = (
+    df.groupby('Delay')['Order Profit Per Order'].agg(
+        mean_profit='mean',
+        total_profit='sum',
+        order_count='count'
+    ).reset_index()
+)
+
+# some deliveries are delay by 2-4 days and some deliveries are before time but the mean_profit is same ~22
+# print(profit_metrics.head(10))
+
+
+# now, finding out the delay distribution
+delay_distribution = (
+    df['Delay'].value_counts(normalize=True).sort_values()*100
+).reset_index()
+
+# delivery late by 3 days have 3.91% and by 4 days have 3.87% and 0 days have 21.21%
+# print(delay_distribution)
+
+
+# now, create a visualization for delay_distribution
+# delay_distribution.columns = ['Delay_Days', 'Percentage']
+
+# print('\nProfit Metrics by Delay Days :')
+# display(profit_metrics.round(1))
+
+# print('\nDelay Distribution :')
+# display(delay_distribution.round(1))
+
+# fig, (ax1,ax2) = plt.subplots(1,2,figsize=(16,6))
+
+# first subplot: delay distribution
+# sns.barplot(x="Delay_Days", y="Percentage", data=delay_distribution, color=accent_color,ax=ax1)
+# ax1.set_title('Delay Distribution')
+# ax1.set_xlabel('Delay (Days)')
+# ax1.set_ylabel('percentage of orders (%)')
+
+
+# second subplot : profit analysis by delay distribution
+# ax2.set_ylabel("Total Profit", color=primary_color)
+# ax2.bar(profit_metrics['Delay'], profit_metrics['total_profit'], color=primary_color, label="Total Profit")
+# ax2.tick_params(axis='y',labelcolor=primary_color)
+
+# ax3 = ax2.twinx()
+
+# ax3.set_xlabel("Delay Days")
+# ax3.set_ylabel("Mean Profit", color=accent_color)
+# ax3.plot(
+#     profit_metrics['Delay'], 
+#     profit_metrics['mean_profit'], 
+#     color = accent_color, 
+#     marker='o',
+#     label="Mean Profit"
+# )
+# ax3.tick_params(axis='y', labelcolor=accent_color)
+
+# helper format function 
+def format_fun(value,tick_number):
+    if value >= 1e6:
+        return f'${value*1e-6:.1f}M'
+    elif value >= 1e3:
+        return f'${value*1e-3:.1f}K'
+    else:
+        return f'${value:.0f}'
+
+# ax2.yaxis.set_major_formatter(ticker.FuncFormatter(format_fun))
+
+# lines, labels = ax3.get_legend_handles_labels()
+# lines2, labels2 = ax2.get_legend_handles_labels()
+
+
+# ax3.legend(lines+lines2, labels+labels2, loc = 'upper right')
+# ax3.grid(True, linestyle=':', alpha=0.5)
+# plt.title('Profit Analysis by Delay Days')
+# plt.savefig('figures/profit_analysis_by_delay.png', dpi=300, bbox_inches='tight')
+# plt.tight_layout()
+# plt.show()
+
+
+# now, let's do the bottleneck detection
+
+def compute_delay_percentage_by_category(category):
+    category_df = df.groupby(category).agg(
+        total_orders = ('Delay', 'count'),
+        late_orders = ('Is_Delayed', 'sum')
+    ).reset_index()
+    category_df['delay_percent'] = (category_df['late_orders'] / category_df['total_orders']) * 100
+    category_df = category_df.sort_values('delay_percent', ascending=False).head(10)
+    return category_df
+
+# setting custom categories from dataset columns
+categories = [
+    'Order Region',
+    'Customer Segment',
+    'Shipping0 Mode',
+    'Order Status',
+    'Type',
+    'Department Name'
+]
+
+fig, axes = plt.subplots(2,3, figsize=(16,7), constrained_layout=True)
+axes = axes.flatten()
+
+for ax, category in zip(axes, categories):
+    category_df = compute_delay_percentage_by_category(category)
+    sns.barplot(
+        data=category_df,
+        x='delay_percent',
+        y=category,
+        ax=ax,
+        palette='viridis'
+    )
+    ax.set_title(f'Delay Percentage by {category}', fontsize=10)
+    ax.set_xlabel('')
+    ax.set_ylabel(category)
+    for i, row in category_df.reset_index().iterrows():
+        ax.text(
+            row['delay_percent'] - 15,
+            i, 
+            f"{row['delay_percent']:.1f}%", 
+            va='center', 
+            fontsize=10, 
+            color='white'
+        )
+
+plt.savefig('figures/delay_percentage_by_category.png', dpi=300, bbox_inches='tight')
+plt.show()
+    
